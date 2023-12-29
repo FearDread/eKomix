a6f7923c1fb59b8a083e78ef4ebfae66333e4075
FearDread <ghaptonstall@gmail.com>
2023-12-29 13:26:30 +0000

    --restart project with new react admin template

--- components/footer.tsx
+++
@@ -1,9 +0,0 @@
-export default function Footer() {
-	return (
-		<div className={'container mt-5 mb-4'}>
-			<footer className={'p-2 border-top'}>
-				This is footer.
-			</footer>
-		</div>
-	);
-}
