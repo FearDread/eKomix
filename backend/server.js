e265ada32fb547f1429720e2d911c381514cbe75
FearDread <ghaptonstall@gmail.com>
2024-02-07 06:38:42 -0800

    --add new mongo db module and applied in backend

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,18 @@
 const app = require("./app");
 const dotenv = require("dotenv");
-const connectDB = require("./db/connectDB")
+const DB = require("./db");
 const cloudinary = require("cloudinary");
-
-// Handling Uncaught Execption => anything not defind Uncaught Execption 
 
 process.on("uncaughtException" , (err) =>{
     console.log(`Error , ${err.message}`);
     console.log(`Shutting down the server due to Uncaught Exception`);
     process.exit(1);
 })
-
-
-//config =>
  dotenv.config({path : "backend/config/config.env"})
-// Connect With MongoDB
-connectDB();
 
 
-// conncet with cloudinary
+ // Connect With MongoDB
+DB.run();
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_NAME,
