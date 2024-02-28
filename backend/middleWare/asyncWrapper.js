9190156eb707cdccbba8fb3f9cd1bb23189cd384
FearDread <ghaptonstall@gmail.com>
2024-02-27 21:18:30 -0800

    --clear branch

--- backend/middleWare/asyncWrapper.js
+++
@@ -1,4 +0,0 @@
-// this is wrapeer class take function as parameter and handle try and catch error
-module.exports = (theFunc) => (req, res, next) => {
-  Promise.resolve(theFunc(req, res, next)).catch(next);
-};
