b3e701523670ec33b1b6c70d0811cb0908c7f02c
FearDread <ghaptonstall@gmail.com>
2024-03-06 07:15:43 -0800

    --add new v6 base template

---
+++ src/pages/Landing.jsx
@@ -0,0 +1,48 @@
+import React, { useEffect } from "react";
+import "../styles/Landing.css";
+import { Hero, ProductElement, Stats } from "../components";
+import { useLoaderData, useNavigate } from "react-router-dom";
+import axios from "axios";
+
+
+export const landingLoader = async () => {
+
+    const result = await axios(`http://localhost:4000/fear/api/product`);
+    console.log(result.data.product);
+    return result.data.product;
+
+
+};
+
+const Landing = () => {
+
+  const products = useLoaderData();
+  const navigate = useNavigate();
+  console.log(products);
+  return (
+    <main>
+      <Hero />
+      <Stats />
+
+      <div className="selected-products">
+        <h2 className="text-6xl text-center my-12 max-md:text-4xl text-accent-content">
+          Trending Products
+        </h2>
+        <div className="selected-products-grid max-w-7xl mx-auto">
+          {products && products.map((product) => (
+            <ProductElement
+              key={product._id}
+              id={product.id}
+              title={product.name}
+              image={product.imageUrl}
+              rating={product.rating}
+              price={product.price}
+            />
+          ))}
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default Landing;
